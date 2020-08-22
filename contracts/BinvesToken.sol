4f6bcde70307ee362ce69720a106d096118b845a
quanpeng <adrianpeng1992@gmail.com>
2020-08-21 20:39:47 -0700

    Use openzeppelin contract template

--- contracts/BinvesToken.sol
+++ contracts/BinvesToken.sol
@@ -1,8 +1,9 @@
+// contracts/Box.sol
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.6.0;
 
-import "./ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
 contract BinvesToken is ERC20, ERC20Burnable {
     constructor(uint256 initialSupply) public ERC20("Binves", "BIN") {
