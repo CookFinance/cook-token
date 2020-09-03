51ef83617830db5d3750075f3c5c997322c48fe9
quanpeng <adrianpeng1992@gmail.com>
2020-09-02 22:02:42 -0700

    Modify BinvesToken.test.js to test with upgradable BinvesToken

--- test/BinvesToken.test.js
+++ test/BinvesToken.test.js
@@ -1,20 +1,38 @@
 // Load dependencies
-const { accounts, contract } = require('@openzeppelin/test-environment');
+const { web3 } = require('@openzeppelin/test-environment');
 const { expect } = require('chai');
-
-// Load compiled artifacts
-const BinvesToken = contract.fromArtifact('BinvesToken');
+const { Contracts, ProxyAdminProject, ZWeb3 } = require('@openzeppelin/upgrades');
 
 describe('BinvesToken', function () {
-  const [ owner ] = accounts;
-  const initialSupply = '100000000000000000000';
+  const initialSupply = '100000000000000000000000000';
 
   beforeEach(async function () {
-    // Deploy a new BinvesToken contract for each test
-    this.contract = await BinvesToken.new(initialSupply, { from: owner });
+    // Initialize OpenZeppelin upgrades
+    ZWeb3.initialize(web3.currentProvider);
+
+    // Create an OpenZeppelin project
+    const [binvesTeam] = await ZWeb3.eth.getAccounts();
+    this.binvesTeam = binvesTeam;
+    const project = new ProxyAdminProject('BinvesTokenProject', null, null, {binvesTeam});
+
+    // Deploy a new BinvesToken contract
+    log('Creating an upgradeable instance of BinvesToken');
+    const BinvesToken = Contracts.getFromLocal('BinvesToken');
+    this.contract = await project.createProxy(BinvesToken, {initArgs: [initialSupply]});
+    const address = this.contract.options.address;
+    log(`Contract created at ${address}`);
+
   });
 
   it('Contract owner should have all initial tokens', async function () {
-    expect((await this.contract.balanceOf.call(owner)).toString()).to.equal(initialSupply);
+    expect(await this.contract.methods.balanceOf(this.binvesTeam).call()).to.equal(initialSupply);
   });
 });
+
+
+// Logging
+function log() {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log.apply(this, arguments)
+  }
+}
