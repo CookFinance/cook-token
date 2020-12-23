ee372c8011d779559ec9f3e6066dd155f569baf5
quanpeng <adrianpeng1992@gmail.com>
2020-12-23 15:23:07 -0800

    Add internal _transferTokens function

--- contracts/CookToken.sol
+++ contracts/CookToken.sol
@@ -506,4 +506,15 @@ contract CookToken {
 
         _moveDelegates(currentDelegate, delegatee, delegatorBalance);
     }
+
+    function _transferTokens(address src, address dst, uint96 amount) internal {
+        require(src != address(0), "Uni::_transferTokens: cannot transfer from the zero address");
+        require(dst != address(0), "Uni::_transferTokens: cannot transfer to the zero address");
+
+        balances[src] = sub96(balances[src], amount, "Uni::_transferTokens: transfer amount exceeds balance");
+        balances[dst] = add96(balances[dst], amount, "Uni::_transferTokens: transfer amount overflows");
+        emit Transfer(src, dst, amount);
+
+        _moveDelegates(delegates[src], delegates[dst], amount);
+    }
 }
