d8849201e2f5147cc66b926c49b9a92f07e8b84a
quanpeng <adrianpeng1992@gmail.com>
2020-12-23 15:22:47 -0800

    Add internal _delegate function

--- contracts/CookToken.sol
+++ contracts/CookToken.sol
@@ -496,4 +496,14 @@ contract CookToken {
         }
         return checkpoints[account][lower].votes;
     }
+
+    function _delegate(address delegator, address delegatee) internal {
+        address currentDelegate = delegates[delegator];
+        uint96 delegatorBalance = balances[delegator];
+        delegates[delegator] = delegatee;
+
+        emit DelegateChanged(delegator, currentDelegate, delegatee);
+
+        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
+    }
 }
