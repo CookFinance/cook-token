1a76456b604f701560375079f5e0dd014a62204e
quanpeng <adrianpeng1992@gmail.com>
2020-12-23 10:34:45 -0800

    Add mint function

--- contracts/CookToken.sol
+++ contracts/CookToken.sol
@@ -285,5 +285,29 @@ contract CookToken {
         minter = minter_;
     }
 
+    /**
+     * @notice Mint new tokens
+     * @param dst The address of the destination account
+     * @param rawAmount The number of tokens to be minted
+     */
+    function mint(address dst, uint rawAmount) external {
+        require(msg.sender == minter, "Cook::mint: only the minter can mint");
+        require(block.timestamp >= mintingAllowedAfter, "Cook::mint: minting not allowed yet");
+        require(dst != address(0), "Cook::mint: cannot transfer to the zero address");
 
+        // record the mint
+        mintingAllowedAfter = SafeMath.add(block.timestamp, minimumTimeBetweenMints);
+
+        // mint the amount
+        uint96 amount = safe96(rawAmount, "Cook::mint: amount exceeds 96 bits");
+        require(amount <= SafeMath.div(SafeMath.mul(totalSupply, mintCap), 100), "Cook::mint: exceeded mint cap");
+        totalSupply = safe96(SafeMath.add(totalSupply, amount), "Cook::mint: totalSupply exceeds 96 bits");
+
+        // transfer the amount to the recipient
+        balances[dst] = add96(balances[dst], amount, "Cook::mint: transfer amount overflows");
+        emit Transfer(address(0), dst, amount);
+
+        // move delegates
+        _moveDelegates(address(0), delegates[dst], amount);
+    }
 }
