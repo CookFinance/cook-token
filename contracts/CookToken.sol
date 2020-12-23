d7ee71957aab02a400390ec11ae7f8f0e972e3c5
quanpeng <adrianpeng1992@gmail.com>
2020-12-23 15:24:57 -0800

    Add all util functions

--- contracts/CookToken.sol
+++ contracts/CookToken.sol
@@ -548,4 +548,31 @@ contract CookToken {
 
         emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
     }
+
+    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
+
+    function safe96(uint n, string memory errorMessage) internal pure returns (uint96) {
+        require(n < 2**96, errorMessage);
+        return uint96(n);
+    }
+
+    function add96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
+        uint96 c = a + b;
+        require(c >= a, errorMessage);
+        return c;
+    }
+
+    function sub96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+    function getChainId() internal pure returns (uint) {
+        uint256 chainId;
+        assembly { chainId := chainid() }
+        return chainId;
+    }
 }
