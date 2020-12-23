2dcfebebe508752b3c36dde8482380ed62fe95d7
quanpeng <adrianpeng1992@gmail.com>
2020-12-23 15:21:27 -0800

    Add delegateBySig function

--- contracts/CookToken.sol
+++ contracts/CookToken.sol
@@ -425,4 +425,24 @@ contract CookToken {
     function delegate(address delegatee) public {
         return _delegate(msg.sender, delegatee);
     }
+
+    /**
+     * @notice Delegates votes from signatory to `delegatee`
+     * @param delegatee The address to delegate votes to
+     * @param nonce The contract state required to match the signature
+     * @param expiry The time at which to expire the signature
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function delegateBySig(address delegatee, uint nonce, uint expiry, uint8 v, bytes32 r, bytes32 s) public {
+        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), getChainId(), address(this)));
+        bytes32 structHash = keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry));
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+        address signatory = ecrecover(digest, v, r, s);
+        require(signatory != address(0), "Cook::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "Cook::delegateBySig: invalid nonce");
+        require(now <= expiry, "Cook::delegateBySig: signature expired");
+        return _delegate(signatory, delegatee);
+    }
 }
