c275bd47e2de2ac3995d770feee8f08139403539
quanpeng <adrianpeng1992@gmail.com>
2020-12-23 15:24:17 -0800

    Add internal _writeCheckpoint function

--- contracts/CookToken.sol
+++ contracts/CookToken.sol
@@ -535,4 +535,17 @@ contract CookToken {
             }
         }
     }
+
+    function _writeCheckpoint(address delegatee, uint32 nCheckpoints, uint96 oldVotes, uint96 newVotes) internal {
+        uint32 blockNumber = safe32(block.number, "Cook::_writeCheckpoint: block number exceeds 32 bits");
+
+        if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
+            checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
+        } else {
+            checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
+            numCheckpoints[delegatee] = nCheckpoints + 1;
+        }
+
+        emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+    }
 }
