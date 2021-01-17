3e4ea17de1c8ad479345f9cd61a81d27ece1a20c
quanpeng <adrianpeng1992@gmail.com>
2021-01-16 21:49:41 -0800

    Initialize StakingRewards smart contract

---
+++ contracts/StakingRewards.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.16;
+
+import "openzeppelin-solidity-2.3.0/contracts/math/Math.sol";
+import "openzeppelin-solidity-2.3.0/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity-2.3.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity-2.3.0/contracts/token/ERC20/SafeERC20.sol";
+import "openzeppelin-solidity-2.3.0/contracts/utils/ReentrancyGuard.sol";
+
+// Inheritance
+import "./interfaces/IStakingRewards.sol";
+import "./RewardsDistributionRecipient.sol";
+
+contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, ReentrancyGuard {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+}
