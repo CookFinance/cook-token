a2ee43a8ba995911f060d0668cd8e0566cae2d80
adrian <adrian@cook.finance>
2021-01-26 21:46:39 -0800

    CookToken proxy test: upgrade token contract

--- test/token/CookToken.proxy.spec.ts
+++ test/token/CookToken.proxy.spec.ts
@@ -1,6 +1,6 @@
 import { ethers, upgrades } from "hardhat";
 import chai from "chai";
-import {CookToken} from "../../typechain";
+import {CookToken, CookTokenForUpgradeTest} from "../../typechain";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import {ContractFactory} from "@ethersproject/contracts";
 
@@ -20,5 +20,13 @@ describe("CookToken (proxy)", () => {
     it('retrieve returns a value previously initialized', async function () {
         // Test if the returned value is the same one
         expect((await cookToken.balanceOf(await initialTokenHolder.getAddress())).toString()).to.equal(ethers.utils.parseEther('10000000000'));
+    });
+
+    it('upgrade token contract', async function () {
+        const cookTokenAddress: String = cookToken.address;
+        const CookTokenForUpgradeTest: ContractFactory = await ethers.getContractFactory("CookTokenForUpgradeTest");
+        const cookTokenForUpgradeTest: CookTokenForUpgradeTest = await upgrades.upgradeProxy(cookToken.address, CookTokenForUpgradeTest, { unsafeAllowCustomTypes: true }) as CookTokenForUpgradeTest;
+        expect(await cookTokenForUpgradeTest.upgradeTest()).to.equal("upgrade successfully");
+        expect(cookTokenForUpgradeTest.address).to.equal(cookTokenAddress);
     });
 });
